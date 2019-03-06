1b8bbed49c6fa19264d03852fe151587abfb3224
Faisal Uje <ujefaisal@gmail.com>
2019-03-06 17:10:25 +0700

    lecture-043

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,9 +6,17 @@ use App\Entity\BlogPost;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $this->loadUsers($manager);
@@ -49,7 +57,10 @@ class AppFixtures extends Fixture
         $user->setUsername('admin')
             ->setEmail('admin@blog.co')
             ->setName('Faisal Uje')
-            ->setPassword('admin123');
+            ->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                'admin123'
+            ));
 
         $this->addReference('user_admin', $user);
 
