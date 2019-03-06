7a6ffad5ea4200a5671b21ce6801791585fca2a1
Faisal Uje <ujefaisal@gmail.com>
2019-03-06 15:14:11 +0700

    lecture-041

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -24,6 +24,12 @@ class Comment
     private $content;
 
     /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
+
+    /**
      * @ORM\Column(type="datetime")
      */
     private $published;
@@ -53,6 +59,24 @@ class Comment
     public function setPublished(\DateTimeInterface $published): self
     {
         $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * @return User
+     */
+    public function getAuthor(): User
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param User $author
+     */
+    public function setAuthor(User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
