b9407a78eeaf56b11c1e2ca4960c8cfc9a65f071
Faisal Uje <ujefaisal@gmail.com>
2019-03-06 22:55:47 +0700

    lecture-050

---
+++ src/EventSubscriber/PasswordHashSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: faisaluje
+ * Date: 06/03/19
+ * Time: 21:52
+ */
+
+namespace App\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class PasswordHashSubscriber implements EventSubscriberInterface
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::VIEW => ['hashPassword', EventPriorities::PRE_WRITE]
+        ];
+    }
+
+    public function hashPassword(GetResponseForControllerResultEvent $event)
+    {
+        $user = $event->getControllerResult();
+        $method = $event->getRequest()->getMethod();
+
+        if (!$user instanceof User || Request::METHOD_POST !== $method){
+            return;
+        }
+
+        // It is on User, we need to hash password here
+        $user->setPassword(
+            $this->passwordEncoder->encodePassword($user, $user->getPassword())
+        );
+    }
+}
