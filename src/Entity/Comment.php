025d1ec451da614b4b81c08489f62b09ebf560fb
Faisal Uje <ujefaisal@gmail.com>
2019-03-06 18:12:06 +0700

    lecture-045 BlogPost with Comment relation and fixtures

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -30,6 +30,12 @@ class Comment
     private $author;
 
     /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\BlogPost", inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $blogPost;
+
+    /**
      * @ORM\Column(type="datetime")
      */
     private $published;
@@ -63,20 +69,26 @@ class Comment
         return $this;
     }
 
-    /**
-     * @return User
-     */
     public function getAuthor(): User
     {
         return $this->author;
     }
 
-    /**
-     * @param User $author
-     */
     public function setAuthor(User $author): self
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    public function getBlogPost(): BlogPost
+    {
+        return $this->blogPost;
+    }
+
+    public function setBlogPost(BlogPost $blogPost): self
+    {
+        $this->blogPost = $blogPost;
 
         return $this;
     }
