702e90571d292f5aceebe0dc27b4d7780eaf66b0
Faisal Uje <ujefaisal@gmail.com>
2019-03-12 19:03:36 +0700

    lecture-070 Setting published date automatically (EventSubscriber)

--- src/EventSubscriber/PublishedDateEntitySubscriber.php
+++ src/EventSubscriber/PublishedDateEntitySubscriber.php
@@ -11,6 +11,7 @@ namespace App\EventSubscriber;
 
 use ApiPlatform\Core\EventListener\EventPriorities;
 use App\Entity\AuthoredEntityInterface;
+use App\Entity\PublishedDateEntityInterface;
 use App\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -18,34 +19,24 @@ use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
-class AuthoredEntitySubscriber implements EventSubscriberInterface
+class PublishedDateEntitySubscriber implements EventSubscriberInterface
 {
-    private $tokenStorage;
-
-    public function __construct(TokenStorageInterface $tokenStorage)
-    {
-        $this->tokenStorage = $tokenStorage;
-    }
-
-    public static function getSubscribedEvents()
+   public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::VIEW => ['getAuthenticatedUser', EventPriorities::PRE_WRITE]
+            KernelEvents::VIEW => ['setDatePublished', EventPriorities::PRE_WRITE]
         ];
     }
 
-    public function getAuthenticatedUser(GetResponseForControllerResultEvent $event)
+    public function setDatePublished(GetResponseForControllerResultEvent $event)
     {
         $entity = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
-        /** @var User $author */
-        $author = $this->tokenStorage->getToken()->getUser();
-
-        if(!$entity instanceof AuthoredEntityInterface || Request::METHOD_POST !== $method) {
+        if(!$entity instanceof PublishedDateEntityInterface || Request::METHOD_POST !== $method) {
             return;
         }
 
-        $entity->setAuthor($author);
+        $entity->setPublished(new \DateTime());
     }
 }
