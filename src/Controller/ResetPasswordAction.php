8bb95ae3f7c9f1562f73bcddeaea84950d4862d2
Faisal Uje <ujefaisal@gmail.com>
2019-03-14 15:01:38 +0700

    lecture-085 Implementing custom PasswordReset action

--- src/Controller/ResetPasswordAction.php
+++ src/Controller/ResetPasswordAction.php
@@ -10,14 +10,29 @@ namespace App\Controller;
 
 use ApiPlatform\Core\Validator\ValidatorInterface;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ResetPasswordAction
 {
     private $validator;
+    private $userPasswordEncoder;
+    private $entityManager;
+    private $tokenManager;
 
-    public function __construct(ValidatorInterface $validator)
+    public function __construct(
+        ValidatorInterface $validator,
+        UserPasswordEncoderInterface $userPasswordEncoder,
+        EntityManagerInterface $entityManager,
+        JWTTokenManagerInterface $tokenManager
+    )
     {
         $this->validator = $validator;
+        $this->userPasswordEncoder = $userPasswordEncoder;
+        $this->entityManager = $entityManager;
+        $this->tokenManager = $tokenManager;
     }
 
     public function __invoke(User $data)
@@ -29,6 +44,19 @@ class ResetPasswordAction
 
         $this->validator->validate($data);
 
+        $data->setPassword(
+            $this->userPasswordEncoder->encodePassword($data, $data->getNewPassword())
+        );
+
+        $this->entityManager->flush();
+
+        $token = $this->tokenManager->create($data);
+
+        return new JsonResponse(['token' => $token]);
+
         // Validator is only called after we return the data from this action!
+        // Only hear it check for user current password, but we've just modified it!
+
+        // Entity is persisted automatically, only if validation pass
     }
 }
