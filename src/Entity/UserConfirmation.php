c186344bc1830dc7772145513f4b4d7764fa17c5
Faisal Uje <ujefaisal@gmail.com>
2019-03-19 19:53:45 +0700

    lecture-091 UserConfirmation custom API Resource

---
+++ src/Entity/UserConfirmation.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: faisaluje
+ * Date: 19/03/19
+ * Time: 17:35
+ */
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={
+ *          "post"={
+ *              "path"="/users/confirm"
+ *          }
+ *     },
+ *     itemOperations={}
+ * )
+ */
+class UserConfirmation
+{
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(min=30, max=30)
+     */
+    public $confirmationToken;
+}
