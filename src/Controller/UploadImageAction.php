fbeb34d73f7b60c11069887ec277cf1d0512fc0d
Faisal Uje <ujefaisal@gmail.com>
2019-03-22 15:07:03 +0700

    lecture-102 Implementing custom Action for upload

--- src/Controller/UploadImageAction.php
+++ src/Controller/UploadImageAction.php
@@ -9,7 +9,53 @@
 namespace App\Controller;
 
 
+use App\Entity\Image;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Exception\ValidatorException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 class UploadImageAction
 {
+    private $formFactory;
+    private $entityManager;
+    private $validator;
 
+    public function __construct(
+        FormFactoryInterface $formFactory,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator
+    )
+    {
+        $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
+    }
+
+    public function __invoke(Request $request)
+    {
+        // Create a new Image instance
+        $image = new Image();
+        // Validate the form
+        $form = $this->formFactory->create(null, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Persist the new Image entity
+            $this->entityManager->persist($image);
+            $this->entityManager->flush();
+
+            $image->setFile(null);
+
+            return $image;
+        }
+
+        // Uploading done for us in background by VichUploader
+
+        // throw on validation exception, that means something went wrong during form validation
+        throw new ValidatorException(
+            $this->validator->validate($image)
+        );
+    }
 }
